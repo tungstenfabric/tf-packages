415115ffcd988db17ce6d15d8436fae8549442fa
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-29 19:02:54 +0300

    Enable multi kernel build
    Change-Id: Ieb133c91290ef02e66ce931be6eff7177cb1b8a9

--- utils/get_kvers.sh
+++ utils/get_kvers.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-running_kver=$(uname -r) 
-if [[ -d "/lib/modules/${running_kver}/build" ]]; then
-  # Running kernel's sources are available
-  kvers=${running_kver}
-else
-  # Let's use newest installed version of kernel-devel
-  kvers=$(rpm -q kernel-devel --queryformat="%{buildtime}\t%{VERSION}-%{RELEASE}.%{ARCH}\n" | sort -nr | head -1 | cut -f2)
-fi
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+kvers=$(cat $my_dir/../kernel_dependencies.info | awk -F ":" '{print $1}')
 
 echo ${kvers}
-
