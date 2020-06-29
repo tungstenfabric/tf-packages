532cb7a68b9850f65d0fb50025f1e90c7ce4fb80
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-29 19:02:54 +0300

    Enable multi kernel build
    Change-Id: Ieb133c91290ef02e66ce931be6eff7177cb1b8a9

---
+++ utils/install_kernels.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+KERNELS_REPOSITORY=${KERNELS_REPOSITORY:-}
+
+if [[ -n "$KERNELS_REPOSITORY" ]]; then
+    repoquery_opts="--disablerepo=* --enablerepo=$KERNELS_REPOSITORY"
+fi
+
+kvers=$($my_dir/get_kvers.sh)
+
+for kver in $kvers; do
+    mmp_ver=$(echo "$kver" | awk -F '-' '{print $1}')
+    packages=$(cat $my_dir/../kernel_dependencies.info | grep ^$mmp_ver | awk -F ":" '{print $2}')
+    for package in $packages; do
+        if ! rpm -q ${package}-${kver} 2>&1 > /dev/null ; then
+            sudo rpm -ivh --nodeps --noscripts --test $(repoquery $repoquery_opts --location ${package}-${kver})
+        fi
+    done
+done
