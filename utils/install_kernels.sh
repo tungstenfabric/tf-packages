ab93b6a3ef0e599d6481771a30c18e5eb00a882c
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-08-07 11:38:48 +0300

    pass repositories list for install script
    Change-Id: Ic62f643c15823643f57314d8a18fa882feb76e21

--- utils/install_kernels.sh
+++ utils/install_kernels.sh
@@ -4,20 +4,20 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 source /etc/os-release
+KERNEL_REPOSITORIES_RHEL8=${KERNEL_REPOSITORIES_RHEL8:-"--disablerepo=* --enablerepo=BaseOS"}
+KVERS=$($my_dir/get_kvers.sh)
 
-kvers=$($my_dir/get_kvers.sh)
-
-for kver in $kvers; do
-    k_mmp_ver=$(echo "$kver" | awk -F '-' '{print $1}')
-    packages=$(cat $my_dir/../kernel_dependencies.info | grep ^$k_mmp_ver | awk -F ":" '{print $2}')
-    if [[ "$ID" == "rhel" && "$k_mmp_ver" == "4.18.0" ]]; then
-        repoquery_opts="--disablerepo=* --enablerepo=BaseOS"
+for KVER in $KVERS; do
+    K_MMP_VER=$(echo "$KVER" | awk -F '-' '{print $1}')
+    PACKAGES=$(cat $my_dir/../kernel_dependencies.info | grep ^$K_MMP_VER | awk -F ":" '{print $2}')
+    if [[ "$ID" == "rhel" && "$K_MMP_VER" == "4.18.0" ]]; then
+        REPOQUERY_OPTS="${KERNEL_REPOSITORIES_RHEL8}"
     else
-        repoquery_opts=''
+        REPOQUERY_OPTS=''
     fi
-    for package in $packages; do
-        if ! rpm -q ${package}-${kver} 2>&1 > /dev/null ; then
-            sudo rpm -ivh --nodeps --noscripts $(repoquery $repoquery_opts --location ${package}-${kver})
+    for PACKAGE in $PACKAGES; do
+        if ! rpm -q ${PACKAGE}-${KVER} 2>&1 > /dev/null ; then
+            sudo rpm -ivh --nodeps --noscripts $(repoquery $REPOQUERY_OPTS --location ${PACKAGE}-${KVER})
         fi
     done
 done
