4ee739b1aa3470a9bd4dd330402cf1bd37edeca3
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-08-07 11:38:48 +0300

    pass repositories list for install script
    Change-Id: Ic62f643c15823643f57314d8a18fa882feb76e21

--- utils/install_kernels.sh
+++ utils/install_kernels.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 source /etc/os-release
-
+KERNEL_REPOSITORIES_RHEL8=${KERNEL_REPOSITORIES_RHEL8:-"--disablerepo=* --enablerepo=BaseOS"}
 kvers=$($my_dir/get_kvers.sh)
 
 for kver in $kvers; do
     k_mmp_ver=$(echo "$kver" | awk -F '-' '{print $1}')
     packages=$(cat $my_dir/../kernel_dependencies.info | grep ^$k_mmp_ver | awk -F ":" '{print $2}')
     if [[ "$ID" == "rhel" && "$k_mmp_ver" == "4.18.0" ]]; then
-        repoquery_opts="--disablerepo=* --enablerepo=BaseOS"
+        repoquery_opts="${KERNEL_REPOSITORIES_RHEL8}"
     else
         repoquery_opts=''
     fi
