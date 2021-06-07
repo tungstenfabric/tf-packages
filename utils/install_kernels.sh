0486d3445750deaf23df8c47351d7503c69e1890
Andrey Pavlov <andrey-mp@yandex.ru>
2021-06-07 18:15:10 +0300

    print kernel version/package during installation
    add reaction on errors during package's installation
    
    Change-Id: Id35fdda553039773808995d5e4c9155d92f60b7c

--- utils/install_kernels.sh
+++ utils/install_kernels.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
@@ -8,6 +8,7 @@ KERNEL_REPOSITORIES_RHEL8=${KERNEL_REPOSITORIES_RHEL8:-"--disablerepo=* --enable
 kvers=$($my_dir/get_kvers.sh)
 
 for kver in $kvers; do
+    echo "INFO: installing $kver"
     k_mmp_ver=$(echo "$kver" | awk -F '-' '{print $1}')
     packages=$(cat $my_dir/../kernel_dependencies.info | grep ^$k_mmp_ver | awk -F ":" '{print $2}')
     if [[ "$ID" == "rhel" && "$k_mmp_ver" == "4.18.0" ]]; then
@@ -17,6 +18,7 @@ for kver in $kvers; do
     fi
     for package in $packages; do
         if ! rpm -q ${package}-${kver} 2>&1 > /dev/null ; then
+            echo "INFO: installing package ${package}-${kver}"
             sudo rpm -ivh --nodeps --noscripts --oldpackage $(repoquery $extra_repos --location ${package}-${kver})
         fi
     done
